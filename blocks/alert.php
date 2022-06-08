342f89d8e87c7cc07a9d2694ecad60bdb53b10ed
Oleg Frolov <oleg_frolov@mail.ru>
2022-06-08 17:26:11 +0300

    alert

--- blocks/alert.php
+++ blocks/alert.php
@@ -1,6 +1,10 @@
 <edit header="Всплавающее окно сообщения">
-<div>
+    <div>
         <wb-include wb-src="/modules/yonger/common/blocks/common.inc.php" />
+    </div>
+    <div class="form-group">
+        <label>Заголовок</label>
+        <input class="form-control" type="text" name="title">
     </div>
     <div>
         <wb-module wb="module=editor" name="alert"></wb-module>
@@ -11,6 +15,12 @@
     <div id="modalAlert" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modalLabelLarge">{{title}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
                 <div class="modal-body">
                     <p>{{alert}}</p>
                 </div>
@@ -18,8 +28,8 @@
         </div>
     </div>
     <script wb-app>
-        setTimeout(function(){
+        setTimeout(function() {
             $("#modalAlert").modal("show")
-        },500)
+        }, 500)
     </script>
 </view>
