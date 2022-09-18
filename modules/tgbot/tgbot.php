45f5812c05cc75e91f9356e4caacfca7678cf070
Oleg Frolov <oleg_frolov@mail.ru>
2022-09-19 01:32:51 +0300

    tgbot

--- modules/tgbot/tgbot.php
+++ modules/tgbot/tgbot.php
@@ -14,6 +14,7 @@ class modTgbot
     public $data;
     private $is_command;
     public $sett;
+    private $chatdb = __DIR__.'/tgbot_chatdb.json';
 
     public function __construct($app)
     {
@@ -24,7 +25,7 @@ class modTgbot
         }
 
         $this->app = $app;
-        
+        $mode = $app->vars('_route.mode');
         $data = file_get_contents('php://input');
         $data = json_decode($data, true);
         $data = (array)$data;
@@ -32,22 +33,25 @@ class modTgbot
 
         $this->data = $app->dot($data);
         $this->chat_id = $this->data->get('callback_query') ? $this->data->get('callback_query.message.chat.id') : $this->data->get('message.chat.id');
+        $this->storage();
 
         if ($this->sett['debug'] == 'on') {
             $this->sendMessage(wbJsonEncode($data));
         }
 
-        if ($app->route->mode) {
-                $call = "tgbot_{$app->route->mode}";
-                if (is_callable($call)) {
-                    $msg = $call($this);
-                    print_r($msg);
-                }
+        if ($mode > '' && method_exists($this, $mode)) {
+            $this->$mode();
+        } elseif ($mode > '') {
+            $call = "tgbot_{$mode}";
+            if (is_callable($call)) {
+                $msg = $call($this);
+                print_r($msg);
+            }
         }
 
         if ($this->data->get('callback_query')) {
             $this->callback();
-        } 
+        }
         $this->message();
 
         exit;
@@ -57,22 +61,25 @@ class modTgbot
     {
         $data = (array)$this->data->get('callback_query');
         $this->data = $this->app->dot($data);
-            if ($this->data->get('data.mode')) {
-                $text = $this->data->get('data.mode');
-            } else {
-                $text = $this->data->get('data');
-            }
-            $this->data->set('message.text', $text);
+        if ($this->data->get('data.mode')) {
+            $text = $this->data->get('data.mode');
+        } else {
+            $text = $this->data->get('data');
+        }
+        $this->data->set('message.text', $text);
     }
 
     public function message()
     {
         $app = &$this->app;
-//        file_put_contents(__DIR__.'/log_'.date("j.n.Y").'.log', json_encode($this->data->get('message'))."\n\r", FILE_APPEND);
+        file_put_contents(__DIR__.'/log_'.date("j.n.Y").'.log', json_encode($this->data->get('message'))."\n\r", FILE_APPEND);
         @$text = trim($this->data->get('message.text'));
         foreach ($this->sett['trans'] as $trans) {
-            if (strtolower($trans['phrase']) == strtolower($text)) {
-                $text = $trans['command'];
+            foreach (explode(',', $trans['phrase']) as $phrase) {
+                if (strtolower($trans['phrase']) == strtolower($text)) {
+                    $text = $trans['command'];
+                    break;
+                }
             }
         }
         //$text='/start';
@@ -82,7 +89,7 @@ class modTgbot
             $item = wbTreeFindBranchById($this->sett['menu']['data'], $com);
             if (isset($item['active']) && $item['active'] == 'on') {
                 $reply = '';
-                if (isset($item['data']['image']) && $item['data']['image'][0]['img'] > ' ') {
+                if (@isset($item['data']['image'][0]) && $item['data']['image'][0]['img'] > ' ') {
                     $reply .= '<a href="'.$app->vars('_route.host').$item['data']['image'][0]['img'].'">
                     </a>';
                 }
@@ -90,34 +97,12 @@ class modTgbot
                 if (strip_tags($item['data']['text']) > '') {
                     $reply .= "\n".$item['data']['text'];
                 }
-                foreach (explode(',', $item['data']['buttons']) as $btn) {
-                    $btns[] = ['text'=>$btn, 'callback_data'=>$btn];
-                }
 
-                $reply = str_replace(['<br>','<br/>','<br />'], "\n", $reply);
-                $reply = str_replace(['&nbsp;'], " ", $reply);
-
-                if (!count($btns)) {
-                    $keyboard = ['remove_keyboard' => true];
-                } else {
-                    $keytype = $item['data']['inline'] == 'on' ? 'inline_keyboard' : 'keyboard';
-
-                    $keyboard = [
-                        'resize_keyboard' => true,
-                        'one_time_keyboard' =>true,
-                        $keytype => [
-                            $btns
-                        ]
-                    ];
-                }
-
-                $allowed_tags = ['<b>','<strong>','<u>','<i>','<em>','<u>','<ins>','<s>','<strike>','<del>','<a>','<code>','<pre>'];
+                $keyboard = $this->keyboard($item['data']['buttons']);
 
                 $msg = [
-                    'chat_id' => $this->chat_id,
-                    'text' => strip_tags($reply, $allowed_tags),
-                    'parse_mode' => 'HTML',
-                    'reply_markup' => json_encode($keyboard)
+                    'text' => $reply,
+                    'reply_markup' => $keyboard
                 ];
 
                 $this->send('sendMessage', $msg);
@@ -134,6 +119,70 @@ class modTgbot
             $this->error();
         }
         exit;
+    }
+
+    private function striptags($reply)
+    {
+        $reply = str_replace(['<br>','<br/>','<br />'], "\n", $reply);
+        $reply = str_replace(['&nbsp;'], " ", $reply);
+        $allowed_tags = ['<b>','<strong>','<u>','<i>','<em>','<u>','<ins>','<s>','<strike>','<del>','<a>','<code>','<pre>'];
+        $reply = strip_tags($reply, $allowed_tags);
+        return $reply;
+    }
+
+        private function keyboard($buttons, $inline = true)
+        {
+            foreach (explode(',', $buttons) as $btn) {
+                if ($btn > '') {
+                    $btns[] = ['text'=>$btn, 'callback_data'=>$btn];
+                }
+            }
+
+            if (!count($btns)) {
+                $keyboard = ['remove_keyboard' => true];
+            } else {
+                $keytype = ($inline == true) ? 'inline_keyboard' : 'keyboard';
+                $keyboard = [
+                    'resize_keyboard' => true,
+                    'one_time_keyboard' =>true,
+                    $keytype => [
+                        $btns
+                    ]
+                ];
+            }
+            return json_encode($keyboard);
+        }
+
+        private function cast()
+        {
+            $chatdb = explode(';', file_get_contents($this->chatdb));
+            $chatdb = array_chunk($chatdb, 20);
+            $text = $this->app->vars('_post.text');
+            $inline = $this->app->vars('_post.inline') == 'on' ? true : false;
+            $keyboard = $this->keyboard($this->app->vars('_post.buttons'), $inline);
+            foreach ($chatdb as $ch) {
+                foreach ($ch as $chat_id) {
+                    if ($chat_id > '') {
+                        $this->chat_id = $chat_id;
+                        $msg = ['text' => $text,'reply_markup' => $keyboard];
+                        $this->send('sendMessage', $msg);
+                    }
+                }
+                sleep(1);
+            }
+            exit;
+        }
+
+
+    public function storage($mode = null)
+    {
+        if ($mode == null) {
+            $chatdb = explode(';', file_get_contents($this->chatdb));
+            if (!in_array($this->chat_id, $chatdb)) {
+                $chatdb[] = $this->chat_id;
+                file_put_contents($this->chatdb, implode(';', $chatdb));
+            }
+        }
     }
 
     public function error($text=null, $btns = null)
@@ -170,7 +219,10 @@ class modTgbot
     public function send($method, $response)
     {
         $response['chat_id'] = $this->chat_id;
-        $response['parse_mode'] = 'HTML';
+        if (isset($response['text'])) {
+            $response['parse_mode'] = 'HTML';
+            $response['text'] = $this->striptags($response['text']);
+        }
         $ch = curl_init('https://api.telegram.org/bot' . $this->sett['token'] . '/' . $method);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
